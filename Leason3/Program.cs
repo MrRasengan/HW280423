f96eb5830efdf8d4c7195bede6f50ef395327ea8
Konstantin <kostik-kostik-12@mail.ru>
2023-05-03 22:03:08 +0300

    add Leason3

---
+++ Leason3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int i = 1;
+int number = Prompt("Введите число: ");
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine()!);
+    return value;
+}
+
+FindCoobTable(number);
+
+void FindCoobTable(int x)
+{
+    while(i <= number)
+    {
+        Console.WriteLine("Таблица кубов заданного числа: " + i * i * i);
+        i++;
+    }
+}
+
